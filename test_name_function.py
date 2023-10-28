b50c31f8a87d1e6d2bc5b57503895bf26090a5d3
Antonio Ruiz <3speedemon3@gmail.com>
2023-10-28 15:21:57 +0200

    Añadiendo prueba unitaria

---
+++ test_name_function.py
@@ -0,0 +1,13 @@
+import unittest
+from name_function import get_formatted_name
+
+class NamesTestCase(unittest.TestCase):
+    """Pruebas para 'name_function.py'"""
+
+    def test_first_last_name(self):
+        """¿Funcionan nombres compuestos?"""
+        formatted_name = get_formatted_name('connor', 'macgregor')
+        self.assertEquals(formatted_name, 'Connor Macgregor')
+
+if __name__ == '__main__':
+    unittest.main()
