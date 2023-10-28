f8ee0d52f20568a2b1b989fbfba9871122c8c357
Antonio Ruiz <3speedemon3@gmail.com>
2023-10-28 15:50:58 +0200

    Subiendo nuevos archivos y pruebas

--- city_functions.py
+++ city_functions.py
@@ -1,7 +1,7 @@
 
 def get_formatted_city(city, country):
-    contents= f"{city}, {country}".title()
-    return contents
+    contents= f"{city}, {country}"
+    return contents.title()
 
 def get_formatted_city_people(city, country, people):
     contents= f"{city} - {country} - people {people}".title()
--- test_citys.py
+++ test_citys.py
@@ -2,3 +2,18 @@ import unittest
 from city_functions import get_formatted_city
 from city_functions import get_formatted_city_people
 
+class CitysTestCase(unittest.TestCase):
+    """Pruebas para 'city_functions'"""
+
+    def test_get_formatted_city(self):
+        """¿La salida es correcta capitalizando la primera letra de cada palabra?"""
+        formatted_city = get_formatted_city('madrid', 'spain')
+        self.assertEquals(formatted_city, 'Madrid, Spain')
+
+    def test_get_formatted_city_people(self):
+        """¿La salida es correcta capitalizando la primera letra de cada palabra y los numeros correcta?"""
+        formatted_city_people = get_formatted_city_people('madrid', 'spain', 1000000)
+        self.assertEquals(formatted_city_people, 'Madrid - Spain - People 1000000')
+
+if __name__ == '__main__':
+    unittest.main()
